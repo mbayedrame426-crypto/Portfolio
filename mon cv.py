53723371e9ce3274626e58029142871d3436d533
mbaye drame <mbayedrame426@gmail.com>
2026-02-18 14:36:55 +0100

    Update mon cv.py

--- mon cv.py
+++ mon cv.py
@@ -91,7 +91,7 @@ with col3:
         st.write(f"- {item}")
 st.header("Mes réalisations académiques")
 st.write=[
-"Réalisations de cartes(types de sols des régions de Dakar et Sédhiou;localisation des commune de Syer;Bokhol;Tivaoune Peulh...)"
+"Réalisations de cartes(types de sols des régions de Dakar et Sédhiou;localisation des commune de Syer;Bokhol;Tivaoune Peulh...)"]
 
 
 
